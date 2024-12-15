e4e1348a4201b9871a0d08a6d3f8a07c153d57f8
Anas <163520934+anas-haq@users.noreply.github.com>
2024-12-15 12:56:43 +0600

    Almost_Prime.cpp
    ...

--- YouKnowWho/Phase 1/Part 1/Practice_01/Almost_Prime.cpp
+++ YouKnowWho/Phase 1/Part 1/Practice_01/Almost_Prime.cpp
@@ -60,3 +60,81 @@ int main()
 
     return 0;
 }
+
+
+// bhaiyar code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll test = 1;
+    // cin >> test;
+    auto isPrime = [&](ll n)
+    {
+        ll total_div = 0;
+        for (ll i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                total_div++;
+            }
+        }
+        if (total_div == 2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    };
+    auto alMostPrime = [&](ll n)
+    {
+        ll ans = 0;
+        for (ll i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                if (isPrime(i))
+                {
+                    ans++;
+                }
+            }
+        }
+        if (ans == 2)
+        {
+            return true;
+        }
+        else
+            return false;
+    };
+    auto Anas = [&]()
+    {
+        ll n;
+        cin >> n;
+        ll ans = 0;
+        for (ll i = 1; i <= n; i++)
+        {
+            if (alMostPrime(i))
+            {
+                ans++;
+            }
+        }
+
+        cout << ans;
+    };
+
+    while (test--)
+    {
+        Anas();
+    }
+
+    return 0;
+}
